9c9ddb626c6b819c7b289628b897af853d188b77
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-03-07 18:40:45 +0000

    animation toggling with JS

---
+++ js/pizza.js
@@ -0,0 +1,12 @@
+$(function()  {
+    
+    var boxtop = $('.box-top');
+    
+    $('a#box-btn').click(function() {
+        $('.box-top').toggleClass('open');
+        $('.big-action').toggleClass('hide');
+        $('.small-action').toggleClass('hide');
+        
+    });
+    
+});
