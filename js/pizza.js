83cfd711ffd507feae2ff49cb952c48674d931a8
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-03-08 01:15:54 +0000

    adjusted form position and color

--- js/pizza.js
+++ js/pizza.js
@@ -6,6 +6,7 @@ $(function()  {
         $('.box-top').toggleClass('open');
         $('.big-action').toggleClass('hide');
         $('.small-action').toggleClass('hide');
+        $('.box-form').show();
         
     });
     
