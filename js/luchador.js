1aac9ff724d1b10213360adb20c97a9d85e7a654
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-04-21 05:14:33 +0000

    changed variable names for clarity, added tear hide/show

--- js/luchador.js
+++ js/luchador.js
@@ -1,18 +1,20 @@
 $(function(){
         $('.no-message').hide();
         $('.yes-message').hide();
-        $('.welcome-message').show();
+        $('.initial-message').show();
+        $('.single-tear').hide();
         
     $('#confirmation-btn').click(function() {
         $('.no-message').hide();
-        $('.welcome-message').hide();
+        $('.initial-message').hide();
         $('.yes-message').show();
     });
     
      $('#deny-btn').click(function() {
-        $('.welcome-message').hide();
+        $('.initial-message').hide();
         $('.yes-message').hide();
         $('.no-message').show();
+        $('.')
     });
     
 });
