98cd72b3c8c4b06f29a0006b2a540e669fd8a03d
Kevin Rogers <kevin_t_rogers@hotmail.com>
2017-05-03 04:36:49 +0000

    coordinated flexing animation with button clicks

--- js/luchador.js
+++ js/luchador.js
@@ -4,7 +4,13 @@ $(function(){
     singleDrop= $('.single-tear-drop'),
     noMessage = $('.no-message'),
     yesMessage = $('.yes-message'),
-    initialMessage = $('.initial-message');    
+    initialMessage = $('.initial-message'),
+    leftHand = $('.hand-left'),
+    rightHand = $('.hand-right'),
+    leftFore = $('.fore-arm-left'),
+    rightFore = $('.fore-arm-right'),
+    leftUpper = $('.upper-arm-left'),
+    rightUpper = $('.upper-arm-right');    
         
         singleTear.hide();
         singleDrop.hide();
@@ -19,6 +25,13 @@ $(function(){
         noMessage.hide();
         initialMessage.hide();
         yesMessage.show();
+        
+        leftHand.toggleClass('hand-left-flex');
+        rightHand.toggleClass('hand-right-flex');
+        leftFore.toggleClass('fore-arm-left-flex');
+        rightFore.toggleClass('fore-arm-right-flex');
+        leftUpper.toggleClass('upper-arm-left-flex');
+        rightUpper.toggleClass('upper-arm-right-flex');
     });
     
      $('#deny-btn').click(function() {
@@ -30,6 +43,13 @@ $(function(){
         singleTear.addClass('roll-base');
         singleDrop.show();
         singleDrop.addClass('roll-drop');
+        
+        leftHand.removeClass('hand-left-flex');
+        rightHand.removeClass('hand-right-flex');
+        leftFore.removeClass('fore-arm-left-flex');
+        rightFore.removeClass('fore-arm-right-flex');
+        leftUpper.removeClass('upper-arm-left-flex');
+        rightUpper.removeClass('upper-arm-right-flex');
 
     });
     
